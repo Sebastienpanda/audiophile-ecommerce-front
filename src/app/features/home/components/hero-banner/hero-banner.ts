af753f983262b06e8300a9dd86b99c12605ab03f
Sebastienpanda <sebastienprodev@gmail.com>
2025-07-24 16:50:58 +0200

    feat: finish hero banner

--- src/app/features/home/components/hero-banner/hero-banner.ts
+++ src/app/features/home/components/hero-banner/hero-banner.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
+import {BtnComponent} from '@shared/components/ui/btn/btn';
 
 @Component({
     selector: 'app-hero-banner',
     templateUrl: './hero-banner.html',
     styleUrl: './hero-banner.scss',
+    imports: [
+        BtnComponent
+    ]
 })
 
 export class HeroBanner {
---
+++ src/app/shared/components/ui/btn/btn.ts
@@ -0,0 +1,17 @@
+import {Component, input} from '@angular/core';
+import {ArrowRight, LucideAngularModule} from 'lucide-angular';
+
+@Component({
+    selector: 'app-btn',
+    templateUrl: './btn.html',
+    styleUrl: './btn.scss',
+    imports: [
+        LucideAngularModule
+    ]
+})
+export class BtnComponent {
+    readonly label = input.required<string>()
+    readonly type = input<'primary' | 'secondary' | 'tertiary'>('primary');
+    readonly disabled = input(false);
+    protected readonly ArrowRight = ArrowRight;
+}
